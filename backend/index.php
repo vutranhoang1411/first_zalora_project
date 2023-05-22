ffc5370194745366e33183c842b1edd6a736fee6
Qila8nguyen <otokhung1805@gmail.com>
2023-05-22 12:01:53 +0700

    get product by id, get all suppliers

--- backend/index.php
+++ backend/index.php
@@ -1,70 +1,68 @@
 <?php
-    require __DIR__ . '/vendor/autoload.php';
-    use Phalcon\Autoload\Loader as PLoader;
-    use Phalcon\Mvc\Micro\Collection as MicroCollection;    
-    use Phalcon\Mvc\Micro;
+require __DIR__ . '/vendor/autoload.php';
+
+use Phalcon\Autoload\Loader as PLoader;
+use Phalcon\Mvc\Micro\Collection as MicroCollection;
+use Phalcon\Mvc\Micro;
 
 
-    //load needed module
-    $loader=new PLoader();
-    $loader->setNamespaces(
-        [
-            'MyApp\Repository'=>'repository',
-            'MyApp\Models' => 'models',
-            'MyApp\Controllers' => 'controllers',
-            'MyApp\DI'=>'di',
-        ]
-    );
+//load needed module
+$loader = new PLoader();
+$loader->setNamespaces(
+    [
+        'MyApp\Repository' => 'repository',
+        'MyApp\Models' => 'models',
+        'MyApp\Controllers' => 'controllers',
+        'MyApp\DI' => 'di',
+    ]
+);
 
-    $loader->register();
-    //Load DI container
-    $MyDI=MyApp\DI\MyDI::getMyDI();
-    //router object;
-    $suppliers=new MicroCollection();
-    $suppliers
-        ->setHandler(MyApp\Controllers\SupplierController::class,true)
-        ->setPrefix("/api/supplier")
-        ->get("/","getAllSupplier")
-        ->post("/","newSupplier")
-        ->put("/","updateSupplier")
-        ->delete("/{id:[0-9]+}","deleteSupplier");
+$loader->register();
+//Load DI container
+$MyDI = MyApp\DI\MyDI::getMyDI();
+//router object;
+$suppliers = new MicroCollection();
+$suppliers
+    ->setHandler(MyApp\Controllers\SupplierController::class, true)
+    ->setPrefix("/api/supplier")
+    ->get("/", "getAllSupplier")
+    ->get("/name", "getAllSuppliersName")
+    ->post("/", "newSupplier")
+    ->put("/", "updateSupplier")
+    ->delete("/{id:[0-9]+}", "deleteSupplier");
 
-    $addresses=new MicroCollection();
-    $addresses
-        ->setHandler(MyApp\Controllers\AddressController::class,true)
-        ->setPrefix("/api/address")
-        ->get("/","getAddress")
-        ->put("/","updateAddress")
-        ->delete("/{id:[0-9]+}","deleteAddress")
-        ->post("/","newAddress");
-    $products = new MicroCollection();
-    $products
-        ->setHandler(MyApp\Controllers\ProductController::class,True)
-        ->setPrefix('/api/product')
-        ->get('/', 'getProductsList')
-        ->post("/","addProduct")
-        ->delete("/{id:[0-9]+}",'deleteProduct')
-        ->put("/",'editProduct');
-    $productSupply = new MicroCollection();
-    $productSupply
-        -> setHandler(MyApp\Controllers\ProductSupplyController::class,true)
-        -> setPrefix("/api/productsupply")
-        -> get("/supplier", "getSuppliersOfProduct")
-        -> delete("/{id:[0-9]+}", "deleteProductSupply")
-        -> post("/","newProductSupply");
+$addresses = new MicroCollection();
+$addresses
+    ->setHandler(MyApp\Controllers\AddressController::class, true)
+    ->setPrefix("/api/address")
+    ->get("/", "getAddress")
+    ->put("/", "updateAddress")
+    ->delete("/{id:[0-9]+}", "deleteAddress")
+    ->post("/", "newAddress");
+$products = new MicroCollection();
+$products
+    ->setHandler(MyApp\Controllers\ProductController::class, True)
+    ->setPrefix('/api/product')
+    ->get('/', 'getProductsList')
+    ->post("/", "addProduct")
+    ->delete("/{id:[0-9]+}", 'deleteProduct')
+    ->get("/{id:[0-9]+}", 'getProductById')
+    ->put("/", 'editProduct');
+$productSupply = new MicroCollection();
+$productSupply
+    ->setHandler(MyApp\Controllers\ProductSupplyController::class, true)
+    ->setPrefix("/api/productsupply")
+    ->get("/supplier", "getSuppliersOfProduct")
+    ->delete("/{id:[0-9]+}", "deleteProductSupply")
+    ->post("/", "newProductSupply");
 
-    $app=new Micro($MyDI);
+$app = new Micro($MyDI);
 
-    $app->mount($suppliers);
-    $app->mount($addresses);
-    $app->mount($products);
-    $app->mount($productSupply);
-    $app->notFound(function(){
-        echo "Page not found";
-    });
-    $app->handle($_SERVER["REQUEST_URI"]);
-
-
-    
-
-
+$app->mount($suppliers);
+$app->mount($addresses);
+$app->mount($products);
+$app->mount($productSupply);
+$app->notFound(function () {
+    echo "Page not found";
+});
+$app->handle($_SERVER["REQUEST_URI"]);
